0854fc175ef581b94a0c5b16d5a13f823dc11c8e
Anna Loginova <anna.loginova@quantori.com>
2021-03-15 21:34:00 +0300

    add homework 2

---
+++ AnnaLoginova/homework2/homework2.test.js
@@ -0,0 +1,9 @@
+const RaceEmulator = require("./homework2");
+
+describe('Homework 2 tests', () => {
+
+    it('aaaa', async () => {
+        const raceEmulator = new RaceEmulator();
+        await raceEmulator.startRace();
+    });
+});
